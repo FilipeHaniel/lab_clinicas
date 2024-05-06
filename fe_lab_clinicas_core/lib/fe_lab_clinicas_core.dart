6724901f451092272b12b4bbf41fe33ed1146241
Filipe Haniel <filipe.haniel@gmail.com>
2024-05-06 08:52:37 -0300

    first commit

---
+++ fe_lab_clinicas_core/lib/fe_lab_clinicas_core.dart
@@ -0,0 +1,6 @@
+library fe_lab_clinicas_core;
+
+export 'src/functional_programming/either.dart';
+export 'src/functional_programming/nil.dart';
+export 'src/functional_programming/unit.dart';
+export 'src/helpers/messages.dart';
